454c499c858d6452c702983c80987207d2082dce
Gagan Deep Sharma <luck.gagan@gmail.com>
2019-02-02 20:28:07 +0530

    refactoring

--- contracts/Thankyou.sol
+++ contracts/Thankyou.sol
@@ -11,19 +11,32 @@ contract Thankyou {
   }
 
   mapping (address=> Thank[]) public myThankYouList;
+  mapping (address=> uint) public myThankYouListCount;
   mapping(address => uint) public ThankYouList;
-  
+  uint public ThankYouListCount;
+
   constructor() public {
     owner = msg.sender;
+    ThankYouListCount = 0;
   }
 
   function sayThanks(address fromAddress, address toAddress, string memory name, uint amount) public {    
-      //Validation-1 (to & from address should not be same)
-      //Validation-2 (amount should not be zero)
-      //Validation-3 (from account should have a fair amount to transfer)      
-      Thank memory newThanks = Thank(toAddress, name, amount);
-      myThankYouList[fromAddress].push(newThanks);  
+    //Validation-1 (to & from address should not be same)
+    //Validation-2 (amount should not be zero)
+    //Validation-3 (from account should have a fair amount to transfer)      
+    Thank memory newThanks = Thank(toAddress, name, amount);
       
+    //Maintain the count for a person's thank giving. 
+    if(myThankYouList[fromAddress].length != 0){
+      myThankYouListCount[fromAddress] = myThankYouListCount[fromAddress] + 1;
+    }else {
+      myThankYouListCount[fromAddress] = 1;
+    }
+      
+    //Push element into the sequence. 
+    myThankYouList[fromAddress].push(newThanks);  
+      
+    //Add a count in the Thanks you list. 
     if(ThankYouList[toAddress] != 0)
     {
       uint count = ThankYouList[toAddress];
@@ -32,6 +45,8 @@ contract Thankyou {
     else {
       ThankYouList[toAddress] = 1;
     }
+    
+    ThankYouListCount++;
   }
   
    
